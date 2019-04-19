66603321673e5d83d4c8455a02aacb09648ebf4c
jtexeira <38398296+jtexeira@users.noreply.github.com>
2019-04-19 18:55:32 +0100

    Server now works under high loads(#6)

--- src/server/server.c
+++ src/server/server.c
@@ -10,7 +10,7 @@
 
 typedef struct stock {
     int codigo;
-    int stock;
+    ssize_t stock;
 } Stock;
 
 int initF() {
@@ -39,12 +39,13 @@ char* articleInfo(int id, int* size) {
     Stock s;
     pread(stock, &s, sizeof(Stock), id * sizeof(Stock));
     int artigos = open("../manager/artigos", O_RDONLY);
-    *size = sprintf(buff, "%d %.2f\n", s.stock, getArticlePrice(artigos, id));
+    *size = sprintf(buff, "%zu %.2f\n", s.stock, getArticlePrice(artigos, id));
     close(artigos);
+    close(stock);
     return buff;
 } 
 
-int updateStock(int id, int new_stock) {
+ssize_t updateStock(int id, ssize_t new_stock) {
     int stock = open("stocks", O_RDWR);
     int vendas = open("vendas", O_WRONLY | O_APPEND | O_CREAT, 0700);
     Stock s;
@@ -59,9 +60,11 @@ int updateStock(int id, int new_stock) {
         int artigos = open("../manager/artigos", O_RDONLY);
         double price = getArticlePrice(artigos, id);
         close(artigos);
-        int read = sprintf(buff, "%d %d %.2f\n", id, -new_stock, -new_stock * price);
-        write(vendas, buff, read);
+        int read = sprintf(buff, "%d %zu %.2f\n", id, -new_stock, -new_stock * price);
+        write(vendas, buff, read + 1);
     }
+    close(stock);
+    close(vendas);
     return s.stock;
 }
 
@@ -80,25 +83,28 @@ int main() {
         int rd = open("../pipes/rd", O_RDONLY);
         int wr = open("../pipes/wr", O_WRONLY);
         while(readln(rd, buff, 100)) {
-            id = atoi(strtok(buff, " "));
+            char* ree = buff;
+            id = atoi(strtok(ree, " "));
             char* abc = strtok(NULL, " ");
             if(!abc && buff[0] != '\n') {
                 char* info = articleInfo(id, &size);
                 if(!info)
                     write(wr, "\n", 2); 
                 else 
-                    write(wr, info, size);
+                    write(wr, info, size + 1);
             }
             else if(buff[0] != '\n') {
-                int quant = atoi(abc);
+                ssize_t quant = atoi(abc);
                 quant = updateStock(id, quant);
                 char quanti[100];
-                size = sprintf(quanti, "%d\n", quant);
-                write(wr, quanti, size);
+                size = sprintf(quanti, "%ld\n", quant);
+                write(wr, quanti, size + 1);
             }
             else
                 write(wr, "\n", 2); 
         }
+        close(rd);
+        close(wr);
         unlink("../pipes/rd");
         unlink("../pipes/wr");
         return 0;
