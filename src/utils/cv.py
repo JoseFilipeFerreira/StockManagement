f310c1d6b47663cad30d841c609757590548e79a
JoseFilipeFerreira <36403066+JoseFilipeFerreira@users.noreply.github.com>
2019-04-30 16:14:52 +0100

    fix generator (#14)
    * rerere
    
    * FIXXXX

--- src/utils/cv.py
+++ src/utils/cv.py
@@ -3,9 +3,13 @@ from string import ascii_lowercase
 from random import randint, choice
 
 def main():
-    MAX_ID = 2500
+    n = 25000
     if len(sys.argv) > 1:
-        MAX_ID = int(sys.argv[1])
+        n = int(sys.argv[1])
+    MAX_ID = 2500
+    if len(sys.argv) > 2:
+        MAX_ID = int(sys.argv[2])
+
     for i in range(n):
         c = randint(0,1)
         v = [
